2a6f8c014e4b4d0c21810c2d8a83841c711e965c
Jose Lui Coyotzi Ipatzi <coil811122@gmail.com>
2018-02-16 17:15:25 -0600

    ChromeServers.js

--- js/ChromeServers.js
+++ js/ChromeServers.js
@@ -28,7 +28,7 @@ var Tools = {
         var d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
         var expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; domain="+location.host;
     }
 };
 
