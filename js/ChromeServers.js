0854fe123aedb51ffb817932655d7c84374c975b
Jose Lui Coyotzi Ipatzi <coil811122@gmail.com>
2018-02-16 11:43:55 -0600

    File ChromeServers,js

---
+++ js/ChromeServers.js
@@ -0,0 +1,9 @@
+/**
+ * Project: OneVideo
+ * File: ChromeServers.js
+ * File Created: 16/02/18, 11:43
+ * Author: Jose Luis Coyotzi Ipatzi
+ * Email: jlci811122@gmail.com
+ * User: xlfederalelk0lx
+ * IDE: PhpStorm.
+ */
