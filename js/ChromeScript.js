5751171b650a349db261c69227d88386dd5714ed
Jose Lui Coyotzi Ipatzi <coil811122@gmail.com>
2018-02-16 15:48:07 -0600

    ChromeScript.js

--- js/ChromeScript.js
+++ js/ChromeScript.js
@@ -11,12 +11,15 @@
 var OneVideo = {
     maste: "https://raw.githubusercontent.com/xlfederalelk0lx/OneVideo/master/js/ChromeServers.js",
     __constructor: function () {
-        OneVideo.setCookie("OneVideo.Player",chrome.extension.getURL('player.html'),1);
+        OneVideo.setCookie("OneVideo.Player.Sources","",-1000);
         var tag = document.createElement("script");
         tag.type = "text/javascript";
         tag.src = "//code.jquery.com/jquery-3.3.1.slim.min.js";
         window.document.head.appendChild(tag);
         this.GithubRepo();
+        setTimeout(function () {
+            OneVideo.isPlayerSources();
+        },1000);
     },
     GithubRepo: function () {
         axios.get("https://api.github.com/repos/xlfederalelk0lx/OneVideo/branches").then(function (value) {
@@ -42,11 +45,36 @@ var OneVideo = {
             window.document.head.appendChild(tag);
         });
     },
+    isPlayerSources: function(){
+        var sources = this.getCookie("OneVideo.Player.Sources");
+        if(sources != "" && sources != undefined){
+            alert(1)
+        }else {
+            setTimeout(function () {
+                OneVideo.isPlayerSources();
+            },1000);
+        }
+    },
     setCookie: function (cname, cvalue, exdays) {
         var d = new Date();
         d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
         var expires = "expires=" + d.toUTCString();
         document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+    },
+    getCookie: function (cname) {
+        var name = cname + "=";
+        var decodedCookie = decodeURIComponent(document.cookie);
+        var ca = decodedCookie.split(';');
+        for (var i = 0; i < ca.length; i++) {
+            var c = ca[i];
+            while (c.charAt(0) == ' ') {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) == 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
     }
 };
 
