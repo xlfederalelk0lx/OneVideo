a12c72eeb3b53ce36da4a86a4884904e8c824b35
Jose Lui Coyotzi Ipatzi <coil811122@gmail.com>
2018-02-16 21:08:55 -0600

    ChromeServers.js

--- js/ChromeHosts.js
+++ js/ChromeHosts.js
@@ -10,7 +10,25 @@
 
 var Servers = {
     __constructor: function () {
-        $(document.body).html("hola mundo")
+        if (location.host == "rapidvideo.com" || location.host == "www.rapidvideo.com") {
+            var data = location.toString().split("/");
+            if(data[3] == "e" || data[3] == "v"){
+                this.Rapidvideo();
+            }
+            //Tools.isSources(location.hash.replace("#",""));
+        },
+        Rapidvideo: function () {
+            var poster = $("#videojs").attr("poster");
+            var file = $("#videojs source").attr("src");
+            if(file != ''){
+                var re = /rapidvideo\.com\/(\.*)/gi;
+                var qualities = $("body").html().match(re);
+                this.sources.push({file:file,label:"360p",type:"video/mp4",default:true});
+                var player = {sources:this.sources};
+                alert(player)
+                //location.href = "https://"+location.host+"#"+escape(JSON.stringify(player));
+            }
+        }
     }
 };
 
