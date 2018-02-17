dfad8367f06ac3ab35fbd6ff036c6a7dd354822c
Jose Lui Coyotzi Ipatzi <coil811122@gmail.com>
2018-02-16 20:37:14 -0600

    ChromeHosts.js

---
+++ js/ChromeHosts.js
@@ -0,0 +1,11 @@
+/**
+ * Project: OneVideo
+ * File: ChromeHosts
+ * File Created: 16/02/18, 20:36
+ * Author: Jose Luis Coyotzi Ipatzi
+ * Email: jlci811122@gmail.com
+ * User: xlfederalelk0lx
+ * IDE: PhpStorm.
+ */
+
+alert(0)
