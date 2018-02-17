c3b7f3c0ef9570d0482ee33a19ae46c4c8ec30e2
Jose Lui Coyotzi Ipatzi <coil811122@gmail.com>
2018-02-16 19:44:54 -0600

    ChromeServers.js

--- js/ChromeServers.js
+++ js/ChromeServers.js
@@ -50,7 +50,7 @@ var Servers = {
             var qualities = $("body").html().match(re);
             this.sources.push({file:file,label:"360p",type:"video/mp4",default:true});
             var player = {sources:this.sources};
-            location.href = "https://"+location.host+"?player="+JSON.stringify(player);
+            location.href = "https://"+location.host+"#"+JSON.stringify(player);
         }
     }
 };
